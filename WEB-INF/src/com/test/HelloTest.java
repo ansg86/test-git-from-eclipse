9d8589208c4aa6285af967123f87840b30351c61
swee guan <ansg86@gmail.com>
2016-06-27 22:32:39 +0800

    modified

--- WEB-INF/src/com/test/HelloTest.java
+++ WEB-INF/src/com/test/HelloTest.java
@@ -5,6 +5,6 @@ public class HelloTest {
 	public static void main(String args[])
 	{
 		System.out.println("TEST LOCAL");
-		System.out.println("SECOND TEST");
+		System.out.println("SECOND TEST--");
 	}
 }
